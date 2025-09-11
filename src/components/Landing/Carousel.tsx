d7cfe836cd73cde6b63ad0794d6f69635f0dab81
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-09-11 23:42:04 +0700

    feat: selector

--- src/components/Landing/Carousel.tsx
+++ src/components/Landing/Carousel.tsx
@@ -1,24 +1,66 @@
 "use client";
 
-import { Carousel, CarouselContent, CarouselItem } from "@ui/carousel";
+import { useEffect, useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "@ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import { Circle } from "lucide-react";
+import { cn } from "@/libs/utils";
 
 export const LandingCarousel = () => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [currentIdx, setCurrentIdx] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    api.on("scroll", () => {
+      setCurrentIdx(api.selectedScrollSnap());
+    });
+  }, [api, currentIdx]);
+
+  const setCarouselIndex = (idx: number) => {
+    if (!api) {
+      return;
+    }
+
+    setCurrentIdx(idx);
+    api.scrollTo(idx);
+  };
+
   return (
     <>
-      <Carousel plugins={[Autoplay({ delay: 4000 })]}>
+      <Carousel setApi={setApi} plugins={[Autoplay({ delay: 15 * 1000 })]}>
         <CarouselContent>
-          <CarouselItem>
+          <CarouselItem key="1">
             <div className="h-[calc(100vh-3.75rem)] w-full bg-gray-300" />
           </CarouselItem>
-          <CarouselItem>
+          <CarouselItem key="2">
             <div className="h-[calc(100vh-3.75rem)] w-full bg-gray-400" />
           </CarouselItem>
-          <CarouselItem>
+          <CarouselItem key="3">
             <div className="h-[calc(100vh-3.75rem)] w-full bg-gray-500" />
           </CarouselItem>
         </CarouselContent>
       </Carousel>
+      <div className="absolute bottom-3 left-1/2 flex gap-x-4">
+        {[0, 1, 2].map((idx) => (
+          <button key={idx} onClick={() => setCarouselIndex(idx)}>
+            <Circle
+              className={cn(
+                `${currentIdx === idx ? "fill-app-yellow" : "fill-transparent"}`,
+                "text-app-yellow hover:fill-app-background hover:text-app-background h-4 w-4 transition-colors"
+              )}
+            />
+          </button>
+        ))}
+      </div>
     </>
   );
 };
