7b067ae7663334c911572554e88a7f867f85768e
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-09-12 10:23:34 +0700

    chore: change bg-color

--- src/components/Landing/TopCarouselCard.tsx
+++ src/components/Landing/TopCarouselCard.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export const TopCarouselCard = ({ children }: { children?: ReactNode }) => {
   return (
-    <div className="rounded-md bg-black/20 p-5 backdrop-blur-xl">
+    <div className="rounded-md bg-black/10 p-5 backdrop-blur-xl">
       {children}
     </div>
   );
