ca06ccff0c3152079af608c92da856234a0b3ae4
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-09-16 08:37:13 +0700

    fix: authentication

--- src/libs/authentication.ts
+++ src/libs/authentication.ts
@@ -1,22 +1,6 @@
 import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
-
-interface JwtPayload {
-  exp: number;
-}
 
 export const isAuthenticated = (): boolean => {
-  const token = Cookies.get("token");
-  if (!token) {
-    return false;
-  }
-
-  try {
-    const decoded = jwtDecode<JwtPayload>(token);
-
-    const isValid = decoded.exp * 1000 > Date.now();
-    return isValid;
-  } catch (error) {
-    return false;
-  }
+  const authenticated = Cookies.get("authenticated");
+  return authenticated === "true";
 };
