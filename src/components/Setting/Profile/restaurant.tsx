f304f9b02f55de983cf039dc13021b933ebe2c5e
Nantapong Ruangsuksriwong <teamnantapong@gmail.com>
2025-10-21 09:48:14 +0700

    fix: restaurant

--- src/components/Setting/Profile/restaurant.tsx
+++ src/components/Setting/Profile/restaurant.tsx
@@ -26,7 +26,7 @@ import {
   updateCustomerProfileMutation,
 } from "@/queries/profile";
 
-export function RestaurantProfilePage() {
+export function DriverProfilePage() {
   return (
     <main className="bg-app-background flex h-full flex-col items-center pt-8">
       <div className="mx-auto w-full max-w-5xl px-4 md:px-6 lg:px-8">
@@ -36,7 +36,7 @@ export function RestaurantProfilePage() {
   );
 }
 
-export function CustomerProfileFormCard() {
+export function RestaurantProfileFormCard() {
   return (
     <section className="rounded-3xl border border-black/10 bg-white shadow-[0_15px_40px_rgba(64,56,49,0.08)]">
       <div className="px-4 py-6 md:px-8 md:py-8">
@@ -113,14 +113,14 @@ const CustomerProfileForm = () => {
       const formData = new FormData();
       formData.append("file", file);
 
-      const response = await fetch("/file/upload", {
+      const response = await fetch("http://localhost:3030/file/upload", {
         method: "POST",
         body: formData,
+        credentials: "include",
       });
 
       if (!response.ok) {
         throw new Error("Failed to upload file");
-        s;
       }
 
       toast.success("File uploaded successfully!");
