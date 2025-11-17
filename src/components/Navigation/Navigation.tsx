b364fbac04586de63e32d443be653a79938d3c80
Kritsakorn Sataratpayoon <zxcnadeems@gmail.com>
2025-11-18 02:28:34 +0700

    add: nav bar history

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Menu, User } from "lucide-react";
-import { LayoutDashboard } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { History, LogOut, User } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { use, useState } from "react";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 import { EditProfileDialog } from "@/components/Navigation/EditProfileDialog";
-import { MenuDialog } from "@/components/Navigation/MenuDialog";
 import { ResetPasswordDialog } from "@/components/Navigation/ResetPasswordDialog";
 import { authenticatedAs } from "@/libs/authentication";
 
@@ -20,9 +19,29 @@ export const Navigation = () => {
   const [isLoginDialogOpen, setIsLoginDialogOpen] = useState<boolean>(false);
   const [isEditProfileDialogOpen, setIsEditProfileDialogOpen] =
     useState<boolean>(false);
-  const [isMenuDialogOpen, setIsMenuDialogOpen] = useState<boolean>(false);
   const [isResetPasswordDialogOpen, setIsResetPasswordDialogOpen] =
     useState<boolean>(false);
+
+  const { logout } = useAuth();
+  const logoutMutation = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      toast.success("Logged Out");
+      location.reload();
+    },
+    onError: () => {
+      toast.error("Failed to logout. Please try again.");
+    },
+  });
+
+  const handleHistoryClick = async () => {
+    const authenticated = await authenticatedAs();
+    if (authenticated) {
+      router.push("/history");
+    } else {
+      toast.error("You have to be logged in to access the history.");
+    }
+  };
 
   const handleProfileClick = async () => {
     const authenticated = await authenticatedAs();
@@ -36,9 +55,9 @@ export const Navigation = () => {
   const handleMenuClick = async () => {
     const authenticated = await authenticatedAs();
     if (authenticated) {
-      setIsMenuDialogOpen(true);
+      logoutMutation.mutate();
     } else {
-      toast.error("You have to be logged in to access the menu.");
+      toast.error("You have to be logged in.");
     }
   };
 
@@ -59,13 +78,15 @@ export const Navigation = () => {
           <BreadcrumbNav />
         </div>
         <div className="flex w-full items-center justify-end">
-          <div className="mr-3 flex gap-x-6"></div>
-          <div className="flex gap-x-4">
+          <div className="mr-5 flex gap-x-6">
+            <button onClick={handleHistoryClick} className="cursor-pointer">
+              <History className="h-[1.75rem] text-white" />
+            </button>
             <button onClick={handleProfileClick} className="cursor-pointer">
               <User className="h-[1.75rem] text-white" />
             </button>
             <button onClick={handleMenuClick} className="cursor-pointer">
-              <Menu className="h-[1.75rem] text-white" />
+              <LogOut className="h-[1.75rem] text-white" />
             </button>
           </div>
         </div>
@@ -83,10 +104,6 @@ export const Navigation = () => {
       <EditProfileDialog
         isEditProfileDialogOpen={isEditProfileDialogOpen}
         setIsEditProfileDialogOpen={setIsEditProfileDialogOpen}
-      />
-      <MenuDialog
-        isMenuDialogOpen={isMenuDialogOpen}
-        setIsMenuDialogOpen={setIsMenuDialogOpen}
       />
     </>
   );
